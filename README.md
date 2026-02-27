53984ce7cb0500f3a542acc424a3b2479ac4effc
Plant619 <sciderx@gmail.com>
2026-02-27 15:11:38 +0800

    feat: create a window (main.js) docs: add setup information

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Weather-app-2
+# Weather App
 Allows users to choose locations and check the current weather there. 
+
+## Setup 
+1. Install required dependencies
+Type ```npm install``` into terminal
+
+2. Run app 
+Type ```npm run start``` into terminal
+
+## Features
---
+++ main.js
@@ -0,0 +1,18 @@
+// importing electron
+const { app, BrowserWindow } = require('electron');
+
+app.whenReady().then(() => {
+
+    // create a window
+    const myWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+
+    // load a webpage
+    myWindow.loadFile('index.html');
+
+})
